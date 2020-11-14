4cc3ee9730365565efecf516094a6963918c7459
hask777 <haskellisp@gmail.com>
2020-11-14 05:48:06 -0500

    heroku init

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Area;
+use App\Competition;
+use App\Game;
+use App\Player;
+use App\Players;
+use App\PlayersByTeam;
+use App\Round;
+use App\Season;
+use App\Team;
+use App\TeamList;
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +28,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('areas', function(){
+    $areas = Area::all();
+    //dump($areas);
+    return $areas;
+});
+
+Route::get('competitions', function(){
+    $competitions = Competition::all();
+    //dump($areas);
+    return $competitions;
+});
+
+Route::get('games', function(){
+    $games = Game::all();
+    //dump($areas);
+    return $games;
+});
+
+Route::get('playersbyteam', function(){
+    $playersByTeam = PlayersByTeam::all();
+    //dump($areas);
+    return $playersByTeam;
+});
+
+Route::get('rounds', function(){
+    $rounds = Round::all();
+    //dump($areas);
+    return $rounds;
+});
+
+Route::get('seasons', function(){
+    $seasons = Season::all();
+    //dump($areas);
+    return $seasons;
 });
